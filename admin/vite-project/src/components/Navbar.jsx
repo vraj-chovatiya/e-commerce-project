d9fb0b9ab3d7e0d89fdcf3d903bc89d361c62ba7
vraj-chovatiya <vrajchovatiya414@gmail.com>
2025-04-27 20:57:36 +0530

    Changes in UI In admin and frontend

--- admin/vite-project/src/components/Navbar.jsx
+++ admin/vite-project/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { assets } from '../assets/assets';
 import '../styles/navbar.css';
+import Logo from '../../../../frontend/src/assets/logo21.png';
 
 const Navbar = ({ setToken }) => {
   return (
-    <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="Logo" />
-      <button onClick={() => setToken('')} className='logout-button'>
-        Log out
-      </button>
-    </div>
+    <nav className='navbar'>
+      <div className="navbar-left">
+        {/* <img className='logo' src={Logo} alt="GoStore Logo" /> */}
+        <h1 className="brand-name">GoStore</h1>
+      </div>
+        <span className='text-gray-500'>Admin Panel</span>
+      <div className="navbar-right">
+        <button onClick={() => setToken('')} className='logout-button'>
+          <span className="logout-icon">↪</span>
+          <span>Log out</span>
+        </button>
+      </div>
+    </nav>
   );
 };
 
